8a4a3fae201f5e0674acc025dd2ea6071dcc87c3
Hs <hanslee358@gmail.com>
2021-08-21 22:24:56 +1000

    enhancement: added links in navbar

--- src/components/elements/Navbar.js
+++ src/components/elements/Navbar.js
@@ -5,23 +5,33 @@ import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
+import { Link } from "@material-ui/core";
 
 export default function Navbar() {
   const classes = useStyles();
+
+  const links = [
+    { name: "Home", href: "#" },
+    { name: "Pages", href: "#" },
+    { name: "Movies & TV Shows", href: "#" },
+    { name: "Blog", href: "#" },
+    { name: "Contact Us", href: "#" },
+  ];
 
   return (
     <div className={classes.root}>
       <AppBar position="fixed">
         <Toolbar className={classes.navbar}>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="menu"
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6">Martin's Movies</Typography>
+          <Typography className={classes.logo} variant="h6">
+            Martin's Movies
+          </Typography>
+          <div className={classes.links}>
+            {links.map((link) => (
+              <Link className={classes.link} href={link.href}>
+                {link.name}
+              </Link>
+            ))}
+          </div>
         </Toolbar>
       </AppBar>
     </div>
@@ -36,8 +46,25 @@ const useStyles = makeStyles((theme) => ({
   menuButton: {
     marginRight: theme.spacing(2),
   },
+  logo: {
+    position: "absolute",
+    left: 20,
+    width: 200,
+  },
   navbar: {
     backgroundColor: "white",
     color: "#A55DCB",
+    display: "flex",
+    justifyContent: "center",
+  },
+  links: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-around",
+    width: 500,
+  },
+  link: {
+    color: "black",
+    textDecoration: "none",
   },
 }));
